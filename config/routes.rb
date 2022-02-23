c986910c7e8f7e7075a6f9cda8a4ab4e87ee84a8
symsk <k.q.t.l.0616@gmail.com>
2022-02-23 19:24:50 +0900

    Add: header、footerを追加/static_pagesディレクトリを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
+  root to: 'static_pages#top'
   resources :messages, param: :uuid
   resources :quizzes
-  # namespace :api do
-  #   resources :messages
-  # end
-  # get '*path', to: 'home#index'
 end
