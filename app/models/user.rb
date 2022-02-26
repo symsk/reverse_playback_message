de78074f1fd4f4e5e8142be1e7da63cd0c60b464
symsk <k.q.t.l.0616@gmail.com>
2022-02-27 00:16:47 +0900

    Add: 管理用のログイン機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
   has_many :quiz, dependent: :destroy
+  validates :email, uniqueness: true, presence: true
+  validates :password, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   root to: 'static_pages#top'
 
-  resources :messages, param: :uuid, only: %i(show new create)
+  resources :messages, param: :uuid, only: %i[show new create]
   resources :quizzes
 
   get 'privacy', to: 'static_pages#privacy'
   get 'terms', to: 'static_pages#terms'
-  resource :contacts, only: %i(new create)
+  resources :users, only: %i[new create]
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  resources :password_resets
 end
