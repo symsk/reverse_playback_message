1821e220db1797e6e34d06f2fe9107412399fe7e
symsk <k.q.t.l.0616@gmail.com>
2022-02-26 21:23:10 +0900

    Improve: 録音機能のリファクタリング

--- app/javascript/packs/recording.js
+++ app/javascript/packs/recording.js
@@ -1,71 +1,70 @@
 let reload = document.getElementById("reload");
-let startButton = document.getElementById("start");
-let stopButton = document.getElementById("stop");
+let startRecording = document.getElementById("start");
+let stopRecording = document.getElementById("stop");
 let playButton = document.getElementById("btn");
 let reverseButton = document.getElementById("reverse_btn");
+const context = new AudioContext();
 let mediaRecorder;
 let localStream;
-const context = new AudioContext();
 let buffer = null;
+let reverse = true;
+let reversed = false;
 chunks = []
 
 reload.onclick = () => { 
   location.reload();
 }
 
-startButton.onclick = function () {
+startRecording.onclick = () => {
   navigator.mediaDevices.getUserMedia({ audio: true })
-      .then(function (stream) {
+      .then((stream) => {
           localStream = stream;
           mediaRecorder = new MediaRecorder(stream);
           mediaRecorder.start();
           console.log("Status: " + mediaRecorder.state);
-      }).catch(function (err) {
+      }).catch((err) => {
           console.log(err);
       });
-      startButton.disabled = true;
+      startRecording.disabled = true;
   }
-  stopButton.onclick = function () {
-      mediaRecorder.stop();
-      console.log("Status: " + mediaRecorder.state);
-      mediaRecorder.ondataavailable = function (event) {
-          if (event.data.size > 0) {
-            chunks.push(event.data);
-          }
-          const blob = new Blob(chunks)
-          const reader = new FileReader()
-          reader.readAsArrayBuffer(blob)
-          reader.onload = function(event) {
-            context.decodeAudioData(reader.result, (b) => buffer = b);
-          }
-          document.getElementById("audio").src = window.URL.createObjectURL(event.data);
-      }
-      localStream.getTracks().forEach(track => track.stop());
-      stopButton.disabled = true;
-  }
-
-  let reverse = true;
-  let reversed = false;
-  reverseButton.onclick = () => {
-      if (reverse !== reversed) {
-        buffer.getChannelData(0).reverse();
-        reversed = !reversed;
-      }
-      const source = context.createBufferSource();
-      source.buffer = buffer;
-      source.playbackRate.value = ((r) => 0 < r ? r : 1)(parseFloat(document.getElementById('rate').value));
-      source.connect(context.destination);
-      source.onended = () => source.stop(0);
-      source.start(0);
-  }
-
-  playButton.onclick = function ()
-  {
-    let id = 'audio';
-    if( typeof( document.getElementById(id).currentTime ) != 'undefined' )
-    {
-      document.getElementById(id).currentTime = 0;
+stopRecording.onclick =  () => {
+    mediaRecorder.stop();
+    console.log("Status: " + mediaRecorder.state);
+    mediaRecorder.ondataavailable = (event) => {
+        if (event.data.size > 0) {
+          chunks.push(event.data);
+        }
+        const blob = new Blob(chunks)
+        const reader = new FileReader()
+        reader.readAsArrayBuffer(blob)
+        reader.onload = () => {
+          context.decodeAudioData(reader.result, (b) => buffer = b);
+        }
+        document.getElementById("audio").src = window.URL.createObjectURL(event.data);
     }
-    
-    document.getElementById(id).play();
+    localStream.getTracks().forEach(track => track.stop());
+    stopRecording.disabled = true;
+}
+
+reverseButton.onclick = () => {
+    if (reverse !== reversed) {
+      buffer.getChannelData(0).reverse();
+      reversed = !reversed;
+    }
+    const source = context.createBufferSource();
+    source.buffer = buffer;
+    source.playbackRate.value = ((r) => 0 < r ? r : 1)(parseFloat(document.getElementById('rate').value));
+    source.connect(context.destination);
+    source.onended = () => source.stop(0);
+    source.start(0);
+}
+
+playButton.onclick =  () => {
+  let id = 'audio';
+  if( typeof( document.getElementById(id).currentTime ) != 'undefined' )
+  {
+    document.getElementById(id).currentTime = 0;
   }
+  
+  document.getElementById(id).play();
+}
