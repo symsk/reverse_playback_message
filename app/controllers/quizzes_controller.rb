7dfe68a8da85cd43b75257ebc8caa3769fa9d2ca
symsk <k.q.t.l.0616@gmail.com>
2022-02-27 13:34:35 +0900

    Improve: 不要なコードを削除

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,6 +1,6 @@
 class QuizzesController < ApplicationController
   before_action :require_login, only: %i[new create]
-  # before_action :require_login, only: %i[new create]
+  
   def index; end
 
   def new; end
