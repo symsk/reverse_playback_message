c970295da2c7bc3468616a31c64f59643470cf77
symsk <k.q.t.l.0616@gmail.com>
2022-03-05 15:05:23 +0900

    Add: メッセージの文字数カウンター機能を追加

---
+++ app/javascript/packs/counter.js
@@ -0,0 +1,12 @@
+  let count = null;
+  let lest = null;
+  let max = 30;
+  let input_area = document.getElementById("text-area");
+  let output_lest = document.getElementById("text-lest");
+
+  input_area.onkeyup = () => {
+    let length = input_area.value.length;
+    count = length;
+    lest =  max - length;
+    output_lest.innerText = lest;
+  }
