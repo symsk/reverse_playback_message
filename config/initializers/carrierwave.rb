74a214a8dc2aa307833eb0b18dcbf70ee7bbd161
symsk <k.q.t.l.0616@gmail.com>
2022-03-27 23:04:18 +0900

    Add: config.fog_public = false を追加

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,6 +6,7 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = ENV['S3_BUCKET_KEY']
+    config.fog_public = false 
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['S3_ACCESS_KEY'],
