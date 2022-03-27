da24523194345085a5a042d1f20d6a49c754434f
symsk <k.q.t.l.0616@gmail.com>
2022-03-27 22:54:19 +0900

    Change: 環境変数を追加

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,12 +5,12 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'reverse-playback-message'
+    config.fog_directory  = ENV['S3_BUCKET_KEY']
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['S3_ACCESS_KEY'],
       aws_secret_access_key: ENV['S3_SECRET_KEY'],
-      region: 'ap-northeast-1',
+      region: ENV['S3_REGION'],
       path_style: true
     }
 end
