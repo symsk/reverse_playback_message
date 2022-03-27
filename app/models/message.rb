9dce15338d00fb86cc02627d3066c9e030b0a9ba
symsk <k.q.t.l.0616@gmail.com>
2022-03-27 12:42:25 +0900

    Add: メッセージ入力項目にひらがなのみのバリデーションを追加

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ApplicationRecord
   validates :message, presence: true, length: { minimum: 1, maximum: 30 }
-  validates :reverse_message, presence: true, length: { minimum: 1, maximum: 40 }
+  HIRAGANA_REGEXP = /\A[ぁ-んー－]+\z/
+  validates :message, format: { with: HIRAGANA_REGEXP }
+  validates :reverse_message, presence: true, length: { minimum: 1, maximum: 100 }
   
   def to_param
     uuid
