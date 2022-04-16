4ece8b59208757a652e430ac0036b72b718e87ce
symsk <k.q.t.l.0616@gmail.com>
2022-04-16 20:10:06 +0900

    Remove: quizzesテーブルのreverse_voiceカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_14_143952) do
+ActiveRecord::Schema.define(version: 2022_04_16_110713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 2022_02_14_143952) do
   create_table "quizzes", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "answer"
-    t.string "reverse_voice"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_quizzes_on_user_id"
