c336333dcde477bfe91494e0b9d4e27a991bf40d
symsk <k.q.t.l.0616@gmail.com>
2022-04-27 21:42:00 +0900

    Improve: インデントを調整

--- app/models/message.rb
+++ app/models/message.rb
@@ -10,23 +10,23 @@ class Message < ApplicationRecord
 
   def reverse_playback_text
     message.to_roma.reverse.
-        gsub(/!|！|g$|m|t|y$|h$|b$|n|/, 
-          "!|！" => "", 
-          "g" => "gu", 
-          "m" => "mu", 
-          "t" => "tu", 
-          "y" => "yu",
-          "h" => "ti",
-          "b" => "bu",
-          "n" => "nn",
-        ).to_kana.
-        gsub(/g|r|k|h|ぢ|s|/,
-          "g" => "ぐ", 
-          "r" => "る",
-          "k" => "く",
-          "h" => "ひ",
-          "ぢ" => "でぃ",
-          "s" => "す",
-        ).delete("a""b""c""d""e""f""g""h""i""j""k""l""m""n""o""p""q""r""s""t""u""v""w""x""y""z")
+    gsub(/!|！|g$|m|t|y$|h$|b$|n|/, 
+      "!|！" => "", 
+      "g" => "gu", 
+      "m" => "mu", 
+      "t" => "tu", 
+      "y" => "yu",
+      "h" => "ti",
+      "b" => "bu",
+      "n" => "nn",
+    ).to_kana.
+    gsub(/g|r|k|h|ぢ|s|/,
+      "g" => "ぐ", 
+      "r" => "る",
+      "k" => "く",
+      "h" => "ひ",
+      "ぢ" => "でぃ",
+      "s" => "す",
+    ).delete("a""b""c""d""e""f""g""h""i""j""k""l""m""n""o""p""q""r""s""t""u""v""w""x""y""z")
   end
 end
