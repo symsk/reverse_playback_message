f0957a4df8008ab640facd851830838f8f402a4a
symsk <k.q.t.l.0616@gmail.com>
2022-04-27 21:34:25 +0900

    Improve: 逆再生文字化のロジックをcontrollerからmodelに移動

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,25 +10,7 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(message_params.merge(uuid: SecureRandom.uuid))
-    @message.reverse_message = @message.message.to_roma.reverse.
-      gsub(/!|！|g$|m|t|y$|h$|b$|n|/, 
-        "!|！" => "", 
-        "g" => "gu", 
-        "m" => "mu", 
-        "t" => "tu", 
-        "y" => "yu",
-        "h" => "ti",
-        "b" => "bu",
-        "n" => "nn",
-      ).to_kana.
-      gsub(/g|r|k|h|ぢ|s|/,
-        "g" => "ぐ", 
-        "r" => "る",
-        "k" => "く",
-        "h" => "ひ",
-        "ぢ" => "でぃ",
-        "s" => "す",
-      ).delete("a""b""c""d""e""f""g""h""i""j""k""l""m""n""o""p""q""r""s""t""u""v""w""x""y""z")
+    @message.reverse_message = @message.reverse_playback_text
 
       if @message.save
         redirect_to message_path(@message)
