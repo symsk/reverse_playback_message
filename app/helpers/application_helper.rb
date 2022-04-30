fab71dd64166b2cee864a9cd413549e5094f29a7
symsk <k.q.t.l.0616@gmail.com>
2022-04-30 13:10:15 +0900

    Change: OGPの設定を変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,8 @@ module ApplicationHelper
         { href: image_url('icon.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
       ],
       og: {
+        title: :title,
+        description: :description,
         type: 'website',
         url: request.original_url,
         image: image_url('ogp.png'),
