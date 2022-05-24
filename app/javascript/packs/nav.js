558dc4933860f680f8d39e620fcfd820fff63125
symsk <k.q.t.l.0616@gmail.com>
2022-05-24 18:48:14 +0900

    Improve: インデントを調整・アロー関数に統一

--- app/javascript/packs/nav.js
+++ app/javascript/packs/nav.js
@@ -2,9 +2,9 @@ let nav = document.getElementById('nav-wrapper');
 let hamburger = document.getElementById('js-hamburger');
 let blackBg = document.getElementById('js-black-bg');
 
-hamburger.addEventListener('click', function () {
-    nav.classList.toggle('open');
+hamburger.addEventListener('click', () => {
+  nav.classList.toggle('open');
 });
-blackBg.addEventListener('click', function () {
-    nav.classList.remove('open');
+blackBg.addEventListener('click', () => {
+  nav.classList.remove('open');
 });
