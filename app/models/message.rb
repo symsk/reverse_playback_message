e37851559a22ee706bce73a79e0adc04e3c98d89
symsk <k.q.t.l.0616@gmail.com>
2022-06-01 16:01:16 +0900

    Change: 作成したgem 'reverse_playback_word'を使用して逆再生文字を生成する記述に変更

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,26 +7,4 @@ class Message < ApplicationRecord
   def to_param
     uuid
   end
-
-  def reverse_playback_text
-    message.to_roma.reverse.
-    gsub(/!|！|g$|m|t|y$|h$|b$|n|/, 
-      "!|！" => "", 
-      "g" => "gu", 
-      "m" => "mu", 
-      "t" => "tu", 
-      "y" => "yu",
-      "h" => "ti",
-      "b" => "bu",
-      "n" => "nn",
-    ).to_kana.
-    gsub(/g|r|k|h|ぢ|s|/,
-      "g" => "ぐ", 
-      "r" => "る",
-      "k" => "く",
-      "h" => "ひ",
-      "ぢ" => "でぃ",
-      "s" => "す",
-    ).delete((/\A[a-z]+\z/).to_s)
-  end
 end
