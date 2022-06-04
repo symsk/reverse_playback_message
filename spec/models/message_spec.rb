d623f5a416d41eb0d7936f02895f9a4fd971cfc9
symsk <k.q.t.l.0616@gmail.com>
2022-06-05 00:45:32 +0900

    Improve: spec/models/message_spec.rbのテストコードに改行を追加

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Message, type: :model do
         expect(message).to be_valid
       end
     end
+
     context "メッセージが50文字のひらがなで入力されている時" do
       it "バリデーションを通過すること" do
         message = Message.new(message: "あ" * 50, reverse_message: "あ" * 50)
@@ -15,6 +16,7 @@ RSpec.describe Message, type: :model do
       end
     end
   end
+
   describe "異常系" do
     context "メッセージが空白の時" do
       it "メッセージの作成が失敗する" do
@@ -23,6 +25,7 @@ RSpec.describe Message, type: :model do
         expect(message.errors.full_messages).to include("Messageを入力してください", "Messageは1文字以上で入力してください")
       end
     end
+
     context "メッセージが1文字以上のカタカナで入力されている時" do
       it "メッセージの作成が失敗する" do
         message = Message.new(message: "ヨロシク", reverse_message: "")
@@ -30,6 +33,7 @@ RSpec.describe Message, type: :model do
         expect(message.errors.full_messages).to include("Messageは不正な値です")
       end
     end
+
     context "メッセージが1文字以上のローマ字で入力されている時" do
       it "メッセージの作成が失敗する" do
         message = Message.new(message: "yoroshiku", reverse_message: "")
