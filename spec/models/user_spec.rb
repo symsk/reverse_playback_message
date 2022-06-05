bf90033e5ea890f0bf62bb7df848aa481afd99d1
symsk <k.q.t.l.0616@gmail.com>
2022-06-05 15:21:12 +0900

    Improve: インデントを調整

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "正常系" do
     context "emailとpasswordが存在する時" do
-      it"userが正常に作成されること" do
+      it "userが正常に作成されること" do
         user = User.new(email: "user@example.com", password: "password")
         expect(user).to be_valid
       end
@@ -12,7 +12,7 @@ RSpec.describe User, type: :model do
 
   describe "異常系" do
     context "emailとpasswordが空の時" do
-      it"userが作成されないこと" do
+      it "userが作成されないこと" do
         user = User.new(email: "", password: "")
         user.valid?
         expect(user.errors.full_messages).to include("Emailを入力してください", "Passwordを入力してください")
