699db308b66978f65dc5013edb93f38920084809
symsk <k.q.t.l.0616@gmail.com>
2022-06-05 14:49:25 +0900

    Add: spec/models/user_spec.rbのテストコードを記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "正常系" do
+    context "emailとpasswordが存在する時" do
+      it"userが正常に作成されること" do
+        user = User.new(email: "user@example.com", password: "password")
+        expect(user).to be_valid
+      end
+    end
+  end
+
+  describe "異常系" do
+    context "emailとpasswordが空の時" do
+      it"userが作成されないこと" do
+        user = User.new(email: "", password: "")
+        user.valid?
+        expect(user.errors.full_messages).to include("Emailを入力してください", "Passwordを入力してください")
+      end
+    end
+  end
 end
